2d7a4679f88a1961b62dd99c29101c76c6ad760e
Aruden <84431560+ArudenKun@users.noreply.github.com>
2025-03-30 15:57:44 +0800

    Improved AppDbContext scalability

--- src/Application/Common/Interfaces/Data/IAppDbContext.cs
+++ src/Application/Common/Interfaces/Data/IAppDbContext.cs
@@ -1,17 +1,8 @@
-﻿using System.Data;
-using Domain.Entities;
+﻿using Domain.Entities;
 
 namespace Application.Common.Interfaces.Data;
 
-public interface IAppDbContext : IDisposable
+public interface IAppDbContext : IAppDbContextBase
 {
-    IDbConnection Connection { get; }
-
-    /// <summary>Open DB connection</summary>
-    void OpenConnection();
-
-    /// <summary>Open DB connection and Begin transaction</summary>
-    IDbTransaction BeginTransaction();
-
     IRepository<Transaction> Transactions { get; }
 }
--- src/Infrastructure/Data/AppDbContext.cs
+++ src/Infrastructure/Data/AppDbContext.cs
@@ -4,55 +4,10 @@ using Domain.Entities;
 
 namespace Infrastructure.Data;
 
-internal class AppDbContext : IAppDbContext
+internal sealed class AppDbContext : AppDbContextBase, IAppDbContext
 {
-    /// <summary>
-    ///     DB Connection for internal use
-    /// </summary>
-    protected IDbConnection InnerConnection { get; }
-
-    /// <summary>
-    ///     Constructor
-    /// </summary>
     public AppDbContext(IDbConnection connection)
-    {
-        InnerConnection = connection;
-    }
+        : base(connection) { }
 
-    public virtual IDbConnection Connection
-    {
-        get
-        {
-            OpenConnection();
-            return InnerConnection;
-        }
-    }
-
-    public void OpenConnection()
-    {
-        if (
-            InnerConnection.State != ConnectionState.Open
-            && InnerConnection.State != ConnectionState.Connecting
-        )
-            InnerConnection.Open();
-    }
-
-    public virtual IDbTransaction BeginTransaction()
-    {
-        return Connection.BeginTransaction();
-    }
-
-    /// <summary>
-    ///     Close DB connection
-    /// </summary>
-    public void Dispose()
-    {
-        if (InnerConnection.State != ConnectionState.Closed)
-            InnerConnection.Close();
-    }
-
-    private IRepository<Transaction>? _transactions;
-
-    public IRepository<Transaction> Transactions =>
-        _transactions ??= new Repository<Transaction>(Connection);
+    public IRepository<Transaction> Transactions => Set<Transaction>();
 }
